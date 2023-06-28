c02300338ce43d56e0bd7e225b972c586a12a04c
PanWidmo <tomasz.baranww@gmail.com>
2023-06-28 11:14:14 +0200

    Move Header & Footer to another dir

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,13 +1,13 @@
-import styles from '@/components/Footer/Footer.module.scss';
+import styles from '@/layout/Footer/Footer.module.scss';
 
 export const Footer = () => {
-    const today = new Date();
+    const currYear = new Date().getFullYear();
 
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__main}>
                 <div className={styles.footer__content}>
-                    <p>PanWidmo, Copyright &copy; {today.getFullYear()}</p>
+                    <p>PanWidmo, Copyright &copy; {currYear}</p>
                 </div>
             </div>
         </footer>
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,10 +1,10 @@
-import styles from '@/components/Header/Header.module.scss';
+import styles from '@/layout/Header/Header.module.scss';
 export const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.header__main}>
                 <div className={styles.header__content}>
-                    <h1>PROJECT NAME</h1>
+                    <h1>Elite Bank</h1>
                 </div>
             </div>
         </header>
