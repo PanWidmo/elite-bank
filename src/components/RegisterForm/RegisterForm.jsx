8ec384063a4ba56f7b09e14cfc1a1dc4effba508
PanWidmo <tomasz.baranww@gmail.com>
2023-07-04 20:47:50 +0200

    Add more fields when register new user

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -7,15 +7,19 @@ import { routes } from '@/services/routes.jsx';
 import { FormField } from '@/components/FormField/FormField.jsx';
 import { useState } from 'react';
 import { useAuth } from '@/hooks/useAuth.jsx';
+import { useDatabase } from '@/hooks/useDatabase.jsx';
 
 const schema = yup.object().shape({
+    name: yup.string().required('Value is required.'),
     email: yup.string().required('Value is required.'),
     password: yup.string().required('Value is required.').min(6, 'Password should be at least 6 characters'),
     password_confirm: yup.string().oneOf([yup.ref('password')], 'Passwords must match'),
+    age: yup.number().min(18, 'You must be an adult to create bank account'),
 });
 
 export const RegisterForm = () => {
     const { createUser } = useAuth();
+    const { addUserToDb } = useDatabase();
     const navigate = useNavigate();
     const [error, setError] = useState('');
 
@@ -29,12 +33,17 @@ export const RegisterForm = () => {
         defaultValues: {
             email: '',
             password: '',
+            password_confirm: '',
+            age: '',
         },
     });
 
     const onSubmit = async (data) => {
         try {
-            await createUser(data.email, data.password);
+            const {
+                user: { uid },
+            } = await createUser(data.email, data.password);
+            await addUserToDb(uid, data);
             navigate(routes.dashboard, { replace: true });
         } catch ({ message }) {
             setError(message);
@@ -43,6 +52,7 @@ export const RegisterForm = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <FormField label="Name" id="name" name="name" type="text" register={register} error={errors.name?.message} />
             <FormField label="Email" id="email" name="email" type="email" register={register} error={errors.email?.message} />
             <FormField label="Password" id="password" name="password" type="password" register={register} error={errors.password?.message} />
             <FormField
@@ -53,6 +63,7 @@ export const RegisterForm = () => {
                 register={register}
                 error={errors.password_confirm?.message}
             />
+            <FormField label="Age" id="age" name="age" type="number" register={register} error={errors.age?.message} />
 
             <button type="submit">Register</button>
             {error}
