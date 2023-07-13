17559d3209e51b4c10e27edba802e1bb9dc3f262
PanWidmo <tomasz.baranww@gmail.com>
2023-07-13 10:38:27 +0200

    Change name of FormField to AuthFormField

---
+++ src/components/AuthFormField/AuthFormField.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import styles from '@/components/AuthFormField/AuthFormField.module.scss';
+import { HiOutlineMail } from 'react-icons/hi';
+import { AiOutlineUser } from 'react-icons/ai';
+import { HiOutlineLockClosed } from 'react-icons/hi';
+import { AiTwotoneCalendar } from 'react-icons/ai';
+
+export const AuthFormField = ({ label, id, name, type = 'text', register, error, ...rest }) => {
+    return (
+        <div className={styles.authFormField}>
+            <label className={styles.authFormField__label} htmlFor={id}>
+                {label}
+            </label>
+            <div className={styles.authFormField__innerWrapper}>
+                {type === 'email' && <HiOutlineMail className={styles.authFormField__icon} />}
+                {type === 'password' && <HiOutlineLockClosed className={styles.authFormField__icon} />}
+                {type === 'text' && <AiOutlineUser className={styles.authFormField__icon} />}
+                {type === 'text' && <AiOutlineUser className={styles.authFormField__icon} />}
+                {type === 'number' && <AiTwotoneCalendar className={styles.authFormField__icon} />}
+
+                <input
+                    className={styles.authFormField__input}
+                    placeholder={`Enter ${label}`}
+                    type={type}
+                    name={name}
+                    id={id}
+                    {...register(name)}
+                    {...rest}
+                />
+            </div>
+
+            {error && <span className={styles.authFormField__error}>*{error}</span>}
+        </div>
+    );
+};
+
+AuthFormField.propTypes = {
+    label: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    register: PropTypes.func,
+    error: PropTypes.string,
+};
