fd1e3a583b65ee08b715447a45c98126cc8866e2
PanWidmo <tomasz.baranww@gmail.com>
2023-07-13 10:37:47 +0200

    Create ActionFormField component

---
+++ src/components/ActionFormField/ActionFormField.jsx
@@ -0,0 +1,33 @@
+import styles from '@/components/ActionFormField/ActionFormField.module.scss';
+import PropTypes from 'prop-types';
+
+export const ActionFormField = ({ label, id, name, type, register, placeholder = 'Enter value', error, ...rest }) => {
+    return (
+        <div className={styles.actionFormField}>
+            <input
+                className={styles.actionFormField__input}
+                placeholder={placeholder}
+                type={type}
+                name={name}
+                id={id}
+                {...register(name)}
+                {...rest}
+            />
+            {error && <span className={styles.actionFormField__error}>*{error}</span>}
+
+            <label className={styles.actionFormField__label} htmlFor={id}>
+                {label}
+            </label>
+        </div>
+    );
+};
+
+ActionFormField.propTypes = {
+    label: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    register: PropTypes.func,
+    placeholder: PropTypes.string,
+    error: PropTypes.string,
+};
