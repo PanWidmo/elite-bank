6774c91f485fe576e3dfeb6f6ee70d035d609798
PanWidmo <tomasz.baranww@gmail.com>
2023-08-01 17:53:46 +0200

    Improve TransferMoney module

--- src/components/TransferMoney/TransferMoney.jsx
+++ src/components/TransferMoney/TransferMoney.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { ActionBox } from '@/components/ActionBox/ActionBox.jsx';
@@ -5,14 +6,19 @@ import { ActionForm } from '@/components/ActionForm/ActionForm.jsx';
 import { ActionFormField } from '@/components/ActionFormField/ActionFormField.jsx';
 import { useForm } from 'react-hook-form';
 import { useDatabase } from '@/hooks/useDatabase.jsx';
+import { ErrorPopup } from '@/components/ErrorPopup/ErrorPopup.jsx';
 
 const schema = yup.object().shape({
-    transfer_to: yup.string().required('Value is required.'),
-    amount: yup.string().required('Value is required.').min(1, 'Value must be greater than or equal to 1'),
+    user_transfer: yup.string().required('Value is required.'),
+    amount_transfer: yup
+        .number()
+        .min(1, 'Value must be greater than or equal to 1')
+        .transform((value) => (isNaN(value) ? 0 : value)),
 });
 
 export const TransferMoney = () => {
-    const { transferMoney } = useDatabase();
+    const [error, setError] = useState('');
+    const { userBalance, transferMoney } = useDatabase();
 
     const {
         register,
@@ -23,33 +29,47 @@ export const TransferMoney = () => {
         resolver: yupResolver(schema),
         shouldUnregister: true,
         defaultValues: {
-            transfer_to: '',
-            amount: 0,
+            user_transfer: '',
+            amount_transfer: '',
         },
     });
 
-    const onSubmit = async (data) => {
+    const onSubmit = async ({ user_transfer, amount_transfer }) => {
         try {
-            await transferMoney(data.transfer_to, Number(data.amount));
-            reset();
-        } catch (error) {
-            console.error(error);
+            if (Number(amount_transfer) > userBalance) {
+                throw new Error(`You don't have money to transfer`);
+            } else {
+                await transferMoney(user_transfer, Number(amount_transfer));
+                reset();
+            }
+        } catch ({ message }) {
+            setError(message);
+            console.error(message);
         }
     };
     return (
-        <ActionBox title="Transfer money">
+        <ActionBox title="Transfer money" type="transfer">
             <ActionForm onSubmit={handleSubmit(onSubmit)}>
                 <ActionFormField
                     label="Transfer to"
-                    id="transfer_to"
-                    name="transfer_to"
+                    id="user_transfer"
+                    name="user_transfer"
                     type="email"
                     placeholder="Email"
                     register={register}
-                    error={errors.transfer_to?.message}
+                    error={errors.user_transfer?.message}
                 />
-                <ActionFormField label="Amount" id="amount" name="amount" type="number" min="1" register={register} error={errors.amount?.message} />
+                <ActionFormField
+                    label="Amount"
+                    id="amount_transfer"
+                    name="amount_transfer"
+                    type="number"
+                    placeholder="Value"
+                    register={register}
+                    error={errors.amount_transfer?.message}
+                />
             </ActionForm>
+            {error && <ErrorPopup msg={error} />}
         </ActionBox>
     );
 };
