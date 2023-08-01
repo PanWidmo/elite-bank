11b4adf3d4d39d0c248247fa63ef7298f77d8e4c
PanWidmo <tomasz.baranww@gmail.com>
2023-08-01 17:56:50 +0200

    Add icon to ErrorPopup module

--- src/components/ErrorPopup/ErrorPopup.jsx
+++ src/components/ErrorPopup/ErrorPopup.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import styles from '@/components/ErrorPopup/ErrorPopup.module.scss';
+import { BiError } from 'react-icons/bi';
 
 export const ErrorPopup = ({ msg = 'Something went wrong.' }) => {
     return (
         <div className={styles.errorPopup}>
-            <h2 className={styles.errorPopup__title}>Oops!</h2>
+            <h2 className={styles.errorPopup__title}>
+                <BiError /> Oops!
+            </h2>
             <p className={styles.errorPopup__description}>{msg}</p>
         </div>
     );
